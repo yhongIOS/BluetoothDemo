d454261949cae7112ab4ed40864a0bfbc27a778e
臧其龙 <zangqilong@zangqilongdeMacBook.local>
2015-10-15 11:42:42 +0800

    Initial Commit

---
+++ CoreBluetoothObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreBluetoothObjc
+//
+//  Created by 臧其龙 on 15/10/15.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreBluetoothObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreBluetoothObjc
+//
+//  Created by 臧其龙 on 15/10/15.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
